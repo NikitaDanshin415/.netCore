cef0dc945ebe624c2c11d0cacdfa5db06f5d489e
Nikita <Nikita@DESKTOP-LR6QSUA>
2020-06-22 15:47:15 +0500

    Первый урок

---
+++ First/SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Path.Combine("/App_Data/Introduction.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "Introduction.pdf";
+            return PhysicalFile(file_path, file_type, file_name);
+        }
+    }
+}
